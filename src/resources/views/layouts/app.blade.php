2528875e02152d5fc7a35c9872143f5c17a4167e
yuki1923 <kygarcons0629@gmail.com>
2022-07-20 01:22:33 +0900

    投稿履歴ページの作成

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -38,7 +38,8 @@
                             <a class="dropdown-item text-light"
                                 href="{{ route('user.show', ['id' => auth()->id()]) }}">マイページ編集</a>
                             <a class="dropdown-item text-light" href="#">パスワード変更</a>
-                            <a class="dropdown-item text-light" href="#">過去投稿</a>
+                            <a class="dropdown-item text-light"
+                                href="{{ route('user.post_history', ['id' => auth()->id()]) }}">過去投稿</a>
                             <a class="dropdown-item text-light" href="#">過去返答</a>
                             <a class="dropdown-item text-light" href="{{ route('user.delete_confirm') }}">退会</a>
                         </div>
